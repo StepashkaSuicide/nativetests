4da889c71110fc503ac9a62cf719b80b73ca7954
StepashkaSuicide <churbakov.evgen@mail.ru>
2022-06-02 20:45:57 +0300

    2 l

---
+++ src/02.ts
@@ -0,0 +1,53 @@
+// type AddressType = {
+//     streetTitle: string
+//     city: CityType
+// }
+// type CityType = {
+//     title: string
+//     countryTitle: string
+// }
+// type TechType = {
+//     id: number
+//     title: string
+// }
+// type StudentType = {
+//     id: number
+//     name: string
+//     age: number
+//     isActive: boolean
+//     address: AddressType
+//     technologies: Array<TechType>
+// }
+//
+// const student: StudentType = {
+//     id: 1,
+//     name: 'John',
+//     age: 32,
+//     isActive: false,
+//     address: {
+//         streetTitle: 'Lenin 3',
+//         city: {
+//             title: 'Moscow',
+//             countryTitle: 'Russia'
+//         }
+//     },
+//     technologies: [
+//         {
+//             id: 1,
+//             title: 'HTML',
+//         },
+//         {
+//             id: 2,
+//             title: 'CSS',
+//         },
+//         {
+//             id: 3,
+//             title: 'React',
+//         },
+//     ]
+// }
+//
+// console.log(student.age)
+// console.log(student.name)
+// console.log(student.address.city.title)
+// console.log(student.technologies[2].title)
